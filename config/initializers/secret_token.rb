bed8819a32b9207fdda3a7c0721a10b73bce8d3f
Andy Buntine <bunts@hhd.com.au>
2014-05-20 10:01:50 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiscogsOauth::Application.config.secret_key_base = 'db211a7bcad94b34c73af378886f0606e69e9180e6a3dfaa0e5037a72c6af797f6d625d3074447f9aee0a3dd99bead10ace4ba87f8a1133388837f280b3d2c70'
