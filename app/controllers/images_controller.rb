86e770efdeeddad7947958c6ceb58d9960d2a61a
Andy Buntine <bunts@hhd.com.au>
2014-05-20 14:20:33 +1000

    Removed my keys

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -8,8 +8,8 @@ class ImagesController < ApplicationController
   end
 
   def authenticate
-    app_key      = "nLPklPRYpykkjycrkunw"
-    app_secret   = "fjshPFKjzHUQeFqsjyeWBnmLAnOixRgJ"
+    app_key      = "YOUR_CONSUMER_KEY"
+    app_secret   = "YOUR_CONSUMER_SECRET"
     request_data = @discogs.get_request_token(app_key, app_secret,
                      "http://127.0.0.1:3000/images/callback")
 
