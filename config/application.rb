e74e8fea1d7e1c5edccc1c52230d4fe50e9919e6
Andrew Buntine von Doom <info@bunts.io>
2016-02-03 10:05:26 +1100

    Added ENV update into application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module DiscogsOauth
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    ENV.update(YAML.load_file(File.join(Rails.root, "config", "environment_variables.yml"))[Rails.env])
   end
 end
